7a72d2b3b3a8192b454f8c219c8d7f6df68d85b2
Carlos Valencia <rasfreed@gmail.com>
2022-11-05 14:50:55 -0500

    fisrt commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // eslint-disable-next-line react/style-prop-object
+    <div style={{ width: '40px', height: '40px', backgroundColor: 'red' }}>
+
     </div>
   );
 }
