0ee0dcf8faf1dc1574b93dd3e3d63133539ddfa4
Carlos Valencia <rasfreed@gmail.com>
2022-11-06 23:04:44 -0500

    modal to create contract was added and some styles more

--- src/components/RoomCard/index.jsx
+++ src/components/RoomCard/index.jsx
@@ -1,9 +1,18 @@
+import React, { useState } from "react";
+import Slide from '@mui/material/Slide';
 import { Box, Typography } from "@mui/material";
 import BlockRoundedIcon from '@mui/icons-material/BlockRounded';
 import BedroomParentRoundedIcon from '@mui/icons-material/BedroomParentRounded';
 import CleaningServicesRoundedIcon from '@mui/icons-material/CleaningServicesRounded';
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 const RoomCard = ({ room }) => {
+
+  const [openModal, setOpenModal] = useState(false);
 
    const sx = {
     color: 'white',
@@ -15,6 +24,12 @@ const RoomCard = ({ room }) => {
     free: 'success.light',
     occupied: 'error.light',
     cleaning: 'warning.light',
+  };
+
+  const hover = {
+    free: 'success.main',
+    occupied: 'error.main',
+    cleaning: 'warning.main',
   };
 
   const estado = {
@@ -31,25 +46,68 @@ const RoomCard = ({ room }) => {
 
   const { name, status } = room;
 
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
   return (
-    <Box
-      sx={{
-        m: 2,
-        width: '120px',
-        height: '120px',
-        bgcolor: colors[status],
-        display: 'flex',
-        borderRadius: '6px',
-        justifyContent: 'center',
-        flexDirection: 'column',
-        alignItems: 'center',
-        cursor: 'pointer',
-      }}
-    >
-      {icon[status]}
-      <Typography sx={{ color: 'white' }} variant="h5">{name}</Typography>
-      <Typography sx={{ color: 'white' }} variant="caption">{estado[status]}</Typography>
-    </Box>
+    <>
+      <Box
+        sx={{
+          m: 2,
+          width: '120px',
+          height: '120px',
+          bgcolor: colors[status],
+          display: 'flex',
+          borderRadius: '6px',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          alignItems: 'center',
+          cursor: 'pointer',
+          boxShadow: 3,
+          transition: '0.4s',
+          '&:hover': {
+            bgcolor: hover[status],
+          },
+        }}
+        onClick={() => setOpenModal(true)}
+      >
+        {icon[status]}
+        <Typography sx={{ color: 'white' }} variant="h5">{name}</Typography>
+        <Typography sx={{ color: 'white' }} variant="caption">{estado[status]}</Typography>
+      </Box>
+
+      {/* MODAL MODAL MODAL MODAL MODAL MODAL MODAL MODAL MODAL MODAL MODAL */}
+      <Dialog
+        open={openModal}
+        fullWidth
+        maxWidth="md"
+        onClose={handleCloseModal}
+        TransitionComponent={Transition}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="create-contract"
+      >
+        <DialogTitle id="alert-dialog-title">
+          <Typography variant="h5" sx={{ display: 'flex', alignItems: 'center', gap: 1, }}>
+            Cambiar estado de habitación
+          </Typography>
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            hola
+          </DialogContentText>
+          <DialogContentText id="alert-dialog-description">
+            hola
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseModal}>Cancelar</Button>
+          <Button onClick={handleCloseModal} autoFocus variant="contained">
+            Sí, cambiar
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   )
 }
 
--- src/pages/Rooms/index.jsx
+++ src/pages/Rooms/index.jsx
@@ -1,16 +1,39 @@
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Typography } from "@mui/material";
+import { useState } from "react";
 import RoomCard from "../../components/RoomCard";
 
 const Rooms = () => {
+
   const data = [
     { name: 202, status: 'free' },
     { name: 204, status: 'occupied' },
     { name: 305, status: 'cleaning' },
     { name: 307, status: 'free' },
+    { name: 202, status: 'free' },
+    { name: 204, status: 'occupied' },
+    { name: 305, status: 'cleaning' },
+    { name: 307, status: 'free' },
+    { name: 202, status: 'free' },
+    { name: 204, status: 'occupied' },
+    { name: 305, status: 'cleaning' },
+    { name: 307, status: 'free' },
   ];
+
   return (
-    data.map(room => (
-      <RoomCard room={room} />
-    ))
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+        }}
+      >
+        {data.map(room => (
+          <RoomCard
+            room={room}
+          />
+        ))}
+      </Box>
+    </>
   );
 };
 
